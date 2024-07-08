5e65c13466ebfdf4334dea590cbe0662c102f26c
Samarora034 <sakshamarora034@gmail.com>
2024-07-08 14:26:34 +0530

    added vercel insights

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import Model from './components/Model';
 import Features from './components/Features';
 import HowItWorks from './components/HowItWorks';
 import Footer from './components/Footer';
-
+import { SpeedInsights } from "@vercel/speed-insights/react"
 import * as Sentry from '@sentry/react';
 
 const App = () => {
